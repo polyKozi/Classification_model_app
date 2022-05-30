7b4cc119aff93e9375fa5ade4d270ec1b1b326da
polyKozi <106531788+polyKozi@users.noreply.github.com>
2022-05-30 20:16:27 +0600

    Fixed app and data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,26 @@
 import streamlit as st
+import pandas as pd
+from sklearn import tree
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-def predict():
-    import pickle
-    with open('model_pic_clf.pkl', 'rb') as pkl_file:
-        my_model = pickle.load(pkl_file)
-    import tensorflow as tf
-    import os
-    path_to_zip = tf.keras.utils.get_file('Pets.zip', extract=True)
-    PATH = os.path.join(os.path.dirname(path_to_zip), 'Pets_filtered')
+df_win_pr = pd.DataFrame()
+df_win_pr = pd.read_csv('data/wine_preprocessed.csv', sep=',')
 
-    BATCH_SIZE = 5
-    IMG_SIZE = (224, 224)
+x_useful_col = ["alcohol", "malic_acid", "ash", "alcalinity_of_ash", "magnesium", "total_phenols", "flavanoids", "nonflavanoid_phenols", "proanthocyanins", "color_intensity", "hue", "OD280/OD315_of_diluted wines", "proline"]
+X = df_win_pr[x_useful_col]
+y = df_win_pr.iloc[:, 1].values
 
-    dataset = tf.keras.utils.image_dataset_from_directory(PATH, shuffle=True, batch_size=BATCH_SIZE, image_size=IMG_SIZE)
-    return my_model.predict(dataset)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
 
 def main():
     page = st.sidebar.selectbox("Choose a page", ["Description", "Model"])
@@ -25,7 +32,7 @@ def main():
     elif page == "Model":
         st.title("Model")
         if st.button("Predict"):
-            st.write(predict())
+            st.write(clf.predict(X_test))
 
 if __name__ == "__main__":
     main()
