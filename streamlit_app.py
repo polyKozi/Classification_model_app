efb4b09cebeb6fbf51d82f7278e8b4ef4a8d5336
polyKozi <106531788+polyKozi@users.noreply.github.com>
2022-05-30 19:38:10 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pickle
+import tensorflow as tf
+import os
+
+with open('model_pic_clf.pkl', 'rb') as pkl_file:
+    my_model = pickle.load(pkl_file)
+
+path_to_zip = tf.keras.utils.get_file('Pets.zip', extract=True)
+PATH = os.path.join(os.path.dirname(path_to_zip), 'Pets_filtered')
+
+BATCH_SIZE = 5
+IMG_SIZE = (224, 224)
+
+dataset = tf.keras.utils.image_dataset_from_directory(PATH, shuffle=True, batch_size=BATCH_SIZE, image_size=IMG_SIZE)
 
 def main():
     page = st.sidebar.selectbox("Choose a page", ["Description", "Model"])
@@ -10,7 +24,7 @@ def main():
     elif page == "Model":
         st.title("Model")
         if st.button("Predict"):
-            st.write("This is a cat =^._.^=")
+            st.write(my_model.predict(dataset))
 
 if __name__ == "__main__":
     main()
