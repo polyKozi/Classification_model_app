e5705f9c7fc6c403e0d17ce130dd577e402ddd88
polyKozi <106531788+polyKozi@users.noreply.github.com>
2022-05-30 18:23:14 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ def main():
         st.write("The dataset contains 12501 photos of cats and 12501 photos of dogs. The model predicts the class the photo belongs to.")
     elif page == "Model":
         st.title("Model")
-    if st.button("Predict"):
-        st.write("This is a cat =^._.^=")
+        if st.button("Predict"):
+            st.write("This is a cat =^._.^=")
 
 if __name__ == "__main__":
     main()
